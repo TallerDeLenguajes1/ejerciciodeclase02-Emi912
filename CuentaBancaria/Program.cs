4d9e716df8f8459da059834ee9b8d46aab51ab94
Emi Merchan <emimerchan83@gmail.com>
2021-09-08 17:50:37 -0300

    Ejercicio clase

---
+++ CuentaBancaria/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CuentaBancaria
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+           
+        CajaAhorro cajaAhorro = new CajaAhorro();
+            CuentaCorrienteDolares cuentaDolares = new CuentaCorrienteDolares();
+            CuentaCorrientePesos cuentaPesos = new CuentaCorrientePesos();
+
+            cajaAhorro.Insercion(50000);
+            cuentaDolares.Insercion(50000);
+            cuentaPesos.Insercion(50000);
+
+            cuentaPesos.Extraccion(1000, CuentaCorrientePesos.TipoExtraccion.CajeroAutomatico);
+        }
+    }
+}
